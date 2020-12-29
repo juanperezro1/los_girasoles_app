a65f42d22e71647361baaf6b4a7bdfb406dcfd79
Juan Camilo Perez Rodriguez <67967779+cam0104@users.noreply.github.com>
2020-12-29 13:37:05 -0500

    login

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -5,15 +5,15 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      alignment: Alignment.center,
-      height: size.height / 2,
-      child: Stack(
-        alignment: Alignment.center,
+    return SafeArea(
+        child: Container(
+      width: double.infinity,
+      height: size.height,
+      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 100),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          
           Column(
-            mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               SvgPicture.asset('assets/images/logo.svg'),
               Text(
@@ -23,58 +23,59 @@ class Body extends StatelessWidget {
                     fontSize: 75,
                     color: Color(0xFFF9CD22)),
               ),
-              Text(
-                "EXPRÉSATE CON FLORES",
-                style: TextStyle(
-                    fontFamily: 'Cerebri Sans',
-                    fontSize: 18,
-                    color: Color(0xFFF9CD22)),
-              ),
-              Container(
-                width: size.width * 0.5,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [Color(0xFFFEDC741), Color(0xFFFF4A824)]),
-                      borderRadius: BorderRadius.circular(18)
-                ),
-                child: Column(
-                  children: <Widget>[
-                    FlatButton(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                      onPressed: () {},
-                      child: Text("Iniciar sesión", style: TextStyle(fontFamily: 'Sofia Pro', fontSize: 18, color: Colors.white),),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                
-                width: size.width * 0.5,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [Color(0xFFFEDC741), Color(0xFFFF4A824)]),
-                      borderRadius: BorderRadius.circular(18)
-                ),
-                child: Column(
-                  children: <Widget>[
-                    FlatButton(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 35),
-                      onPressed: () {},
-                      child: Text("Registrarme", style: TextStyle(fontFamily: 'Sofia Pro', fontSize: 18, color: Colors.white),),
-                    )
-                  ],
-                ),
-              )
+              Text("EXPRÉSATE CON FLORES",
+                  style: TextStyle(
+                      fontFamily: 'Cerebri Sans',
+                      fontSize: 18,
+                      color: Color(0xFFF9CD22))),
             ],
-          )
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                  width: size.width * 0.5,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Color(0xFFFEDC741), Color(0xFFFF4A824)]),
+                      borderRadius: BorderRadius.circular(18),
+                      ),
+                  child: FlatButton(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                    onPressed: () {},
+                    child: Text(
+                      "Iniciar sesión",
+                      style: TextStyle(
+                          fontFamily: 'Sofia Pro',
+                          fontSize: 18,
+                          color: Colors.white),
+                    ),
+                  )),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                  width: size.width * 0.5,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xFFFFE4200), width: 2),
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  child: FlatButton(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+                    onPressed: () {},
+                    child: Text(
+                      "Registrarme",
+                      style: TextStyle(
+                          fontFamily: 'Sofia Pro',
+                          fontSize: 18,
+                          color: Color(0xFFF303030)),
+                    ),
+                  )),
+            ],
+          ),
         ],
       ),
-    );
+    ));
   }
 }
